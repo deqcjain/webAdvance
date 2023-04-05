a94032b6d071fd1a2bac5f4e5663142b6e9051ce
Ninja <cjain@deqode.com>
2023-04-05 19:34:01 +0530

    feat(Logout and Email verification)
    User email check from localStorage if already exists or not also  Whenever we register we don't see option for that and same for login, list and logout.

--- script/user.js
+++ script/user.js
@@ -1,9 +1,11 @@
+const userObj = [];
+
+// Registering User
 function registerHandler() {
   const fname = document.getElementById("fname");
   const lname = document.getElementById("lname");
   //gender evaluation
   const genderByName = document.getElementsByName("gender");
-  // console.log(genderByName);
   let gender = null;
   genderByName.forEach((sex) => {
     if (sex.checked) {
@@ -15,40 +17,56 @@ function registerHandler() {
   const email = document.getElementById("email");
 
   const password = document.getElementById("password");
-  if (!validation(fname, lname, gender, username, email, password)) {
+  const roles = document.getElementsByName("roles");
+  let role = null;
+  roles.forEach((rol) => {
+    if (rol.checked) {
+      role = rol.value;
+    }
+  });
+  if (!validation(fname, lname, gender, username, email, password, role)) {
     return;
   }
-
-  // console.log(gender);
+  const userReg = {};
+  userReg["fname"] = fname.value;
+  userReg["lname"] = lname.value;
+  userReg["gender"] = gender.value;
+  userReg["username"] = username.value;
+  userReg["email"] = email.value;
+  userReg["password"] = password.value;
+  userReg["role"] = role.value;
+  userObj.push(userReg);
+  console.log(userObj);
   localStorage.setItem("fname", fname.value);
   localStorage.setItem("lname", lname.value);
   localStorage.setItem("gender", gender);
   localStorage.setItem("username", username.value);
   localStorage.setItem("email", email.value);
   localStorage.setItem("password", password.value);
-
+  localStorage.setItem("role", role);
+  localStorage.setItem("userObj", JSON.stringify(userObj));
   //switching pages to login after registering
   switchPages("login");
 }
 
 //VALIDATION FOR REGISTRATION
-function validation(fname, lname, gender, username, email, password) {
+function validation(fname, lname, gender, username, email, password, role) {
   if (
     fname.value == "" ||
     lname.value == "" ||
     gender == null ||
     username.value == "" ||
     email.value == "" ||
-    password.value == ""
+    password.value == "" ||
+    role == null
   ) {
     fname.classList.add("error");
     fname.classList.add("error");
-    // genderByName[0].classList.add('error')
-    // genderByName[1].classList.add('error')
     lname.classList.add("error");
     username.classList.add("error");
     email.classList.add("error");
     password.classList.add("error");
+    role.classList.add("error");
     alert("Cannot leave any empty field");
     return false;
   }
@@ -69,6 +87,19 @@ function validation(fname, lname, gender, username, email, password) {
       return false;
     }
   }
+
+  const storageObj = localStorage.getItem("userObj");
+
+  const userArr = JSON.parse(storageObj);
+
+    for (let index = 0; index < userArr.length; index++) {
+      if (email.value === userArr[index].email) {
+        email.classList.add("error");
+        alert("Email already exists");
+        return false;
+      }
+    } 
+  
   //password check
   if (password.value.length > 3 || password.value.length < 10) {
     let flag = false;
@@ -97,31 +128,38 @@ function validation(fname, lname, gender, username, email, password) {
 function loginHandler() {
   const email = localStorage.getItem("email");
   const password = localStorage.getItem("password");
-  // console.log(email, password);
 
   const inputEmail = document.getElementById("logEmail");
   const inputPassword = document.getElementById("logPassword");
   if (email === inputEmail.value && password === inputPassword.value) {
+    document.getElementById("aregister").style.display = "none";
+    const listParent = document.getElementById("navList");
+    const liLogout = document.createElement("li");
+    const logout = document.createElement("a");
+    logout.setAttribute("class", "logout");
+    logout.setAttribute("href", "#");
+    logout.setAttribute("onclick", "logoutHandler()");
+    logout.setAttribute("id", "alogout");
+    logout.innerHTML = "Logout";
+    liLogout.appendChild(logout);
+    listParent.appendChild(liLogout);
     switchPages("list");
     listUsers();
   } else {
-    // console.log(inputEmail.value, inputPassword.value);
     alert("email and password are invalid: they do not match");
   }
 }
 
 //List Users
 function listUsers() {
+  document.getElementById("alist").style.display = "block";
   var articles = document.getElementById("content");
-  // console.log(localStorage);
   let i = 0;
   var article = document.createElement("div");
   for (const key in localStorage) {
     if (i === localStorage.length) break;
-    // console.log(localStorage[key]);
     i++;
     var articleName = document.createElement("h3");
-    // articleName.setAttribute("class", "heading");
     const element = localStorage[key];
     articleName.innerText = key + " : " + element;
     article.appendChild(articleName);
@@ -129,16 +167,22 @@ function listUsers() {
   }
 }
 
+//Logout User
+function logoutHandler() {
+  document.getElementById("aregister").style.display = "block";
+  document.getElementById("alogin").style.display = "block";
+  document.getElementById("alogout").style.display = "none";
+  switchPages("register");
+}
+
 //Switch pages
 function switchPages(id2) {
   const currentActive = document.querySelector("nav a.active");
   currentActive.classList.remove("active");
+  currentActive.style.display = "none";
   const className = currentActive.classList[0];
   document.getElementById(className).style.display = "none";
-  // document.getElementsByClassName(id1)[0].classList.remove("active");
-  // page1.style.display="none";
   const page2 = document.getElementById(id2);
   document.getElementsByClassName(id2)[0].classList.add("active");
-  // console.log(page2);
   page2.style.display = "block";
 }
