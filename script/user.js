d47673f221ff56d667def36a9258703ce30d874f
Ninja <cjain@deqode.com>
2023-04-05 14:48:20 +0530

    feat(register user): Registering user and storing details in localStorage
    All user info required during registration is stored in localStorage and then switching to login after submitting the details

--- script/user.js
+++ script/user.js
@@ -19,30 +19,45 @@ links.forEach((link) => {
 function registerHandler() {
 
   const fname = document.getElementById("fname");
-  localStorage.setItem("fname", fname);
+  localStorage.setItem("fname", fname.value);
   const lname = document.getElementById("lname");
-  localStorage.setItem("lname", lname);
-  const gender = document.getElementById("gender");
+  localStorage.setItem("lname", lname.value);
+  //gender evaluation
+  const genderByName = document.getElementsByName("gender");
+  console.log(genderByName);
+  let gender=null;
+  genderByName.forEach((sex)=>{
+     if(sex.checked){
+      gender=sex.value;
+     }
+  })
+  console.log(gender);
   localStorage.setItem("gender", gender);
   const username = document.getElementById("username");
-  localStorage.setItem("username", username);
+  localStorage.setItem("username", username.value);
   const email = document.getElementById("email");
-  localStorage.setItem("email", email);
+  localStorage.setItem("email", email.value);
   const password = document.getElementById("password");
-  localStorage.setItem("password", password);
-  userReg[fname]=fname;
-  userReg[lname]=lname;
-  userReg[gender]=gender;
-  userReg[username]=username;
-  userReg[email]=email;
-  userReg[password]=password;
+  localStorage.setItem("password", password.value);
+
+  userReg['fname']=fname.value;
+  userReg['lname']=lname.value;
+  userReg['gender']=gender;
+  userReg['username']=username.value;
+  userReg['email']=email.value;
+  userReg['password']=password.value;
   console.log(userReg);
+
+  //switching pages to login after registering
   switchPages('register','login')
 }
 
 function switchPages(id1,id2){
   const page1=document.getElementById(id1);
+  document.getElementsByClassName(id1)[0].classList.remove("active");
   page1.style.display="none";
   const page2=document.getElementById(id2);
+  document.getElementsByClassName(id2)[0].classList.add("active")
+  console.log(page2);
   page2.style.display="block";
 }
