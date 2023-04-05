15b7b750588a4f678ecdb14fb7362b6422f28312
Ninja <cjain@deqode.com>
2023-04-05 13:24:20 +0530

    feat(switching pages): switching pages using modular functions
    We can switch pages by clicking on the links provided in the navBar and changing the display

--- script/user.js
+++ script/user.js
@@ -1,40 +1,48 @@
-const links = document.querySelectorAll('nav a');
+const userReg={}
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    const currentActive = document.querySelector('nav a.active');
-    currentActive.classList.remove('active');
-    let className=currentActive.classList[0];
-    document.getElementById(className).style.display='none';
-  
-    className=link.classList[0];
-    link.classList.add('active');
-    document.getElementById(className).style.display='block';
 
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    const currentActive = document.querySelector("nav a.active");
+    currentActive.classList.remove("active");
+    let className = currentActive.classList[0];
+    document.getElementById(className).style.display = "none";
+
+    className = link.classList[0];
+    link.classList.add("active");
+    document.getElementById(className).style.display = "block";
   });
 });
 
+function registerHandler() {
 
-function registerHandler(event) {
-    const registerForm=document.getElementById('registerForm');
-    event.preventDefault();
+  const fname = document.getElementById("fname");
+  localStorage.setItem("fname", fname);
+  const lname = document.getElementById("lname");
+  localStorage.setItem("lname", lname);
+  const gender = document.getElementById("gender");
+  localStorage.setItem("gender", gender);
+  const username = document.getElementById("username");
+  localStorage.setItem("username", username);
+  const email = document.getElementById("email");
+  localStorage.setItem("email", email);
+  const password = document.getElementById("password");
+  localStorage.setItem("password", password);
+  userReg[fname]=fname;
+  userReg[lname]=lname;
+  userReg[gender]=gender;
+  userReg[username]=username;
+  userReg[email]=email;
+  userReg[password]=password;
+  console.log(userReg);
+  switchPages('register','login')
+}
 
-    const formData=new FormData(registerForm);
-    const fname=formData.get('fname');
-    
-    const lname=formData.get('lname');
-    const gender=formData.get('gender');
-    const username=formData.get('username')
-    const email=formData.get('email')
-    const password=formData.get('password')
-
-    localStorage.setItem('fname', fname);
-    localStorage.setItem('lname', lname);
-    localStorage.setItem('gender', gender);
-    localStorage.setItem('username', username);
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-    document.getElementById('register').style.display='none';
-    document.getElementById('login').style.display='block';
-
+function switchPages(id1,id2){
+  const page1=document.getElementById(id1);
+  page1.style.display="none";
+  const page2=document.getElementById(id2);
+  page2.style.display="block";
 }
